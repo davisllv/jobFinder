9d3d37837a33f5f3a10e1c0322eae934375772d9
davisllv <davisllvsnt57@gmail.com>
2022-07-20 22:54:11 -0300

    creating the model

---
+++ models/JobModel.js
@@ -0,0 +1,34 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const db = require("./database/connection");
+
+const Job = db.define("job", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  salary: {
+    type: Sequelize.DECIMAL(18, 2),
+    allowNull: false,
+  },
+  company: {
+    type: Sequelize.STRING(),
+    allowNull: false,
+  },
+
+  email: {
+    type: Sequelize.STRING(),
+    allowNull: false,
+  },
+
+  newJob: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true,
+  },
+});
+
+module.exports = Job;
