f48a07844c6d71a5644fc3ea25350c37b878d6ef
davisllv <davisllvsnt57@gmail.com>
2022-07-20 21:55:34 -0300

    conecting the table

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
-
 const app = express();
+
+const db = require("./database/connection");
 
 app.listen(3333, () => {
   console.log("Servidor Iniciado");
 });
 
+// db conection
+
+db.authenticate()
+  .then(() => {
+    console.log("Conectou");
+  })
+  .catch((err) => console.log("Errors", err));
+
+//routes
 app.get("/", (request, response) => {
   return response.json({ message: "Servidor rodando man" });
 });
